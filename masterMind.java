edefb06d32c1aa71c191ad58b62a90cfb9f01912
pichupikaout <30759044+pichupikaout@users.noreply.github.com>
2020-11-22 18:29:14 -0500

    Add files via upload

---
+++ masterMind.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class masterMind {
+    public static void main () {
+        String color = "kbywrg";
+        char[] code = new char[4];
+        char[] inputs = new char[4];
+        int turns = 0;
+        String xo = "";
+        while (turns < 10) {
+            System.out.println("Turn " + turns+1);  
+            for (int b = 0; b < 4; b++) { //gives out a random code
+                  Random a = new Random();
+                  int randomLettervalue = Math.abs(a.nextInt(color.length()));
+                  char randomLetter = color.charAt(randomLettervalue);
+                  code[b] = randomLetter;
+            
+            
+            }
+            Scanner console = new Scanner(System.in);
+            for (int z = 0; z < inputs.length; z++) {
+                System.out.println("letter number " + z+1 + ": ");
+                inputs[z] = console.next().charAt(0);
+            }
+            for (int n = 0; n < inputs.length; n++) { // checks if you got the color right and is in the right spot
+                if (inputs[n] == code[n]) {
+                    xo = xo + 'x';
+                    inputs[n] = 'N';
+                }
+                
+            }
+            for (int o = 0; o < inputs.length; o++) { // after checking if you got the color right and is in the right spot, this checks if you got the color right, but it's in the wrong spot
+                for (int p = 0; p < code.length; p++) {
+                    if (inputs[o] == code[p]) {
+                        xo = xo + 'o';
+                        inputs[o] = 'N';
+                    }
+                }
+            }
+            System.out.println(xo);
+            System.out.println("--------------------------------------------------------------");
+            turns++;
+            if (xo.equals("xxxx")) {
+                System.out.println("Congrats! You did it in " + (turns + 1) + " turns!");
+                
+            }
+        }
+        if (!xo.equals("xxxx")) { //checks if you didn't guess the code after 10 turns
+            System.out.println("You lose, the code was " + Arrays.toString(code) + ". Better luck next time!");;
+        }
+}
+}
